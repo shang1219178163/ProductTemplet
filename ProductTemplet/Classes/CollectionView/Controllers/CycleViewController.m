e5ee8b18be2adfbd258bd10500498d9f0497925b
Shang <shang1219178163@gmail.com>
2019-09-05 17:36:32 +0800

    完美实现循环滚动

--- ProductTemplet/Classes/CollectionView/Controllers/CycleViewController.m
+++ ProductTemplet/Classes/CollectionView/Controllers/CycleViewController.m
@@ -22,8 +22,6 @@
     [super viewDidLoad];
 //    self.title = @"循环滚动式视图";
     self.view.backgroundColor = [UIColor groupTableViewBackgroundColor];
-//    [self setScrollView];
-//    [self setVerScrollView];
     
     NSArray *urlArray = @[@"http://upload-images.jianshu.io/upload_images/1714291-6c664d526b380115.jpg?imageMogr2/auto-orient/strip%7CimageView2/2/w/700",
                           @"http://upload-images.jianshu.io/upload_images/6486956-a0c75e83255105c9?imageMogr2/auto-orient/strip%7CimageView2/2/w/583",
@@ -55,29 +53,6 @@
         make.height.equalTo(200);
     }];
     
-}
-
-- (void)setScrollView{
-    NSArray *urlArray = @[@"http://upload-images.jianshu.io/upload_images/1714291-6c664d526b380115.jpg?imageMogr2/auto-orient/strip%7CimageView2/2/w/700",
-                          @"http://upload-images.jianshu.io/upload_images/6486956-a0c75e83255105c9?imageMogr2/auto-orient/strip%7CimageView2/2/w/583",
-                          @"http://upload-images.jianshu.io/upload_images/3580598-482508548410c111.jpg?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240"];
-    _horScrollView = [[NNCycleScrollView alloc]initWithFrame:CGRectMake(0, 50, CGRectGetWidth(self.view.bounds), 200)];
-    _horScrollView.imageArray = urlArray;
-    _horScrollView.delegate = self;
-    [self.view addSubview:_horScrollView];
-}
-
-- (void)setVerScrollView{
-    NSArray *imageArray = @[[UIImage imageNamed:@"1"],[UIImage imageNamed:@"3"],[UIImage imageNamed:@"2"]];
-    _verScrollView = [[NNCycleScrollView alloc]initWithFrame:CGRectMake(0, 300, CGRectGetWidth(self.view.bounds), 200)];
-    _verScrollView.direction = UICollectionViewScrollDirectionVertical;
-    _verScrollView.imageArray = imageArray;
-    _verScrollView.delegate = self;
-    _verScrollView.blockDidSelectedIndex = ^(NSInteger index) {
-        NSLog(@"___第%ld个",index);
-
-    };
-    [self.view addSubview:_verScrollView];
 }
 
 #pragma mark - CarouselScrollViewDelegate
