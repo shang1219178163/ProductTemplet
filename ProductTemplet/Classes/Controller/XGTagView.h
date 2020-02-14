b8cc6aa2b1fbbff5ea35c80dd41c328f655545aa
Shang <shang1219178163@gmail.com>
2020-02-14 16:32:41 +0800

    tag标签

--- ProductTemplet/Classes/Controller/XGTagView.h
+++ ProductTemplet/Classes/Controller/XGTagView.h
@@ -12,6 +12,30 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface XGTagView : UIView
 
+/**
+ *  初始化
+ *
+ *  @param frame    frame
+ *  @param tagArray 标签数组
+ *
+ *  @return
+ */
+- (instancetype)initWithFrame:(CGRect)frame tagArray:(NSMutableArray*)tagArray;
+
+// 标签数组
+@property (nonatomic,retain) NSArray* tagArray;
+
+// 选中标签文字颜色
+@property (nonatomic,retain) UIColor* textColorSelected;
+// 默认标签文字颜色
+@property (nonatomic,retain) UIColor* textColorNormal;
+
+// 选中标签背景颜色
+@property (nonatomic,retain) UIColor* backgroundColorSelected;
+// 默认标签背景颜色
+@property (nonatomic,retain) UIColor* backgroundColorNormal;
+
+
 @end
 
 NS_ASSUME_NONNULL_END
